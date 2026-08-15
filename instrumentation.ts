7b83ba2239287699eb0c8fe660f66f3bbb6aae4a
Jason Achin Yakubu <88377613+jaytech504@users.noreply.github.com>
2026-08-15 10:38:51 +0100

    Add instrumentation registration function

---
+++ instrumentation.ts
@@ -0,0 +1,7 @@
+import patchflow from './patchflow';
+
+export function register() {
+  patchflow.init({
+    apiKey: 'pf_live_0ffaf8d1af94b6cf4a6895e2e9a9073460aa961ea267d6809ca47b39c8057daa', host: 'https://patchflow-backend-xax6.onrender.com'
+  });
+}
